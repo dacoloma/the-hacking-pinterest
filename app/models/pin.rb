c4e5fafc88c2ef501fb231199064137a9cbff783
D.Coloma <dnl.coloma@gmail.coma>
2018-02-01 20:10:20 +0100

    validate URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,7 @@ class Pin < ApplicationRecord
   has_many :comments
 
   validates :url, presence: true , length: {maximum: 200}
+# j'ai rajouté une gem pour la validation d'URL  => cf. README.md
   validates :url, url: true
   validates :user_id, presence: true
 
